ee052131c00118a47d7df87b8c648acc54ce27fe
renkarsu <42439654+renkarsu@users.noreply.github.com>
2019-08-12 23:17:01 +0900

    Update Partition-function.cpp

--- dp/Partition-function.cpp
+++ dp/Partition-function.cpp
@@ -21,17 +21,24 @@
 
 template<typename T>
 vector<vector<T>> get_partition(int n, int m){
-
-    vector<vector<T>> dp(n + 1, vector< T >(m + 1));
-
+ 
+    vector<vector<T>> dp(m + 1, vector< T >(n + 1));
+ 
     dp[0][0] = 1;
     for(int i = 1; i <= m; i++){
         for(int j = 0; j <= n; j++){
-
+ 
             if(j - i >= 0) dp[i][j] = dp[i-1][j] + dp[i][j-i];
             else           dp[i][j] = dp[i-1][j];
-
+ 
         }
     }
     return dp;
 }
+/*
+int main() {
+  int n, m;
+  cin >> n >> m;
+  cout << get_partition< modint >(n, m)[m][n] << endl;
+}
+*/
