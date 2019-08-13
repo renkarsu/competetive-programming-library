b9ea58f4f0b53ce1f48a145b57f5bd6d6d949086
renkarsu <42439654+renkarsu@users.noreply.github.com>
2019-08-13 10:23:38 +0900

    Update Partition-function-int.cpp

--- math/Partition-function-int.cpp
+++ math/Partition-function-int.cpp
@@ -29,7 +29,7 @@ vector<vector<T>> get_partition(int n, int m){
     for(int i = 0; i <= n; ++i){
       for(int j = 1; j <= m; ++j){
 
-        if(i-j >= 0) (dp[i][j] = dp[i][j-1] + dp[i-j][j])%MOD;
+        if(i-j >= 0) dp[i][j] = (dp[i][j-1] + dp[i-j][j])%MOD;
         else         dp[i][j] = dp[i][j-1];
 
       }
