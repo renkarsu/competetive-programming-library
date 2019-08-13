97f630b34c6fc06ddd893f9e3e69a5580b641fec
renkarsu <42439654+renkarsu@users.noreply.github.com>
2019-08-13 14:32:51 +0900

    first commit

---
+++ math/Duplicate-combination-dp-aItems.cpp
@@ -0,0 +1,30 @@
+vector<vector<int>> get_nHm(){
+ 
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+ 
+    for(int i = 0; i <= n; ++i){
+        dp[i][0] = 1;
+    }
+    for(int i = 0; i < n; ++i){
+        for(int j = 1; j <= m; ++j){
+            if(j-1-a[i] >= 0){
+                dp[i+1][j] = (dp[i+1][j-1]+dp[i][j]-dp[i][j-1-a[i]]+MOD)%MOD;
+            }
+            else{
+                dp[i+1][j] = (dp[i+1][j-1]+dp[i][j])%MOD;
+            }
+        }
+    }
+    return dp;
+}
+
+//a[i]はi番目の品物の個数
+/*
+int n,m;
+
+int main(){
+cin >> n >> m;
+cout << get(n,m) << endl;
+}
+*/
+//https://atcoder.jp/contests/abc021/submissions/6877827
